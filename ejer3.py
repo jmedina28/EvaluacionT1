55900451e3a82a4dcd8e042708d0cf243ecc4b93
Juan Medina <91565820+jmedina28@users.noreply.github.com>
2022-09-23 14:59:44 +0200

    añadida los pares del 0 al 20

--- ejer3.py
+++ ejer3.py
@@ -8,6 +8,9 @@ def generador(opcion):
     for i in range(-10,1):
       lista.append(i)
   elif opcion == 3:
+    for i in range(0,21,2):
+      lista.append(i)
+  elif opcion == 4:
     for i in range(-20,1):
       if i % 2 != 0:
         lista.append(i)
