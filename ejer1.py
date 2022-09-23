e99cc1d98a689cbcd485a97ed5f0db8780d4d8e0
Juan Medina <91565820+jmedina28@users.noreply.github.com>
2022-09-23 14:51:12 +0200

    terminado el ejercicio 1

--- ejer1.py
+++ ejer1.py
@@ -4,14 +4,18 @@ matriz = []
 fila = 0
 dimensiones = int(input("Introduzca el número de filas que desea que tenga su matriz ix4: "))
 
-
 def generador(dim):
   
   for i in range(dim):
     matriz.append([])
     for j in range(3):
       matriz[i].append(random.randint(0,10))
+
+def elemento(m):
+
+  for p in range(dimensiones):
+    m[p].append(sum(m[p]))
    
-    
 generador(dimensiones)
+elemento(matriz)
 print(matriz)
