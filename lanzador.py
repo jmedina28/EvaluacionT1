faee3e0803aebca410b29508da8dec1987d64418
Juan Medina <91565820+jmedina28@users.noreply.github.com>
2022-09-23 16:20:35 +0200

    carpeta ejercicios

--- lanzador.py
+++ lanzador.py
@@ -1,23 +1,22 @@
 
 
-
 def iniciador():  
     seleccion = int(input("Introduzca el número de ejercicio que desea ejecutar: "))
     if seleccion == 1:
-        import ejer1
+        import ejercicios.ejer1
     elif seleccion == 2:
-        from ejer2 import comprobacion
+        from ejercicios.ejer2 import comprobacion
         cadena = input("Introduzca una frase: ")
         lista = list(cadena)
         print(lista)
         print(comprobacion(lista))
     elif seleccion == 3:
-        from ejer3 import generador2
+        from ejercicios.ejer3 import generador2
         opcion = 1
         for i in range(5):
             print("Opción", opcion, ":", generador2(opcion))
             opcion += 1
     elif seleccion == 4:
-        from tabla import generador3
+        from ejercicios.tabla import generador3
         generador3()
         
