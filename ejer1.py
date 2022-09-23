b3829f6f42a6578b62008a6e13ca00f1f3f754f3
Juan Medina <91565820+jmedina28@users.noreply.github.com>
2022-09-23 15:53:32 +0200

    modificaciones

--- ejer1.py
+++ ejer1.py
@@ -9,14 +9,16 @@ def generador1(dim):
    else:
      matriz.append([])
      generador1(dim-1)
-
-def elemento(matriz):
+f = 0
+def elemento(matriz,f):
     if len(matriz) == 0:
         return
     else:
+        f += 1
+        print("Introduzca los elementos de la fila" , f)
         for i in range(3):
-            matriz[fila].append(random.randint(0,10))
-        elemento(matriz[1:])
+            matriz[fila].append(int(input("Introduzca un elemento: ")))
+        elemento(matriz[1:],f)
 
 def suma(matriz):
     if len(matriz) == 0:
@@ -25,6 +27,6 @@ def suma(matriz):
         matriz[fila].append(matriz[fila][0] + matriz[fila][1] + matriz[fila][2])
         suma(matriz[1:])
 generador1(dimensiones)
-elemento(matriz)
+elemento(matriz,f)
 suma(matriz)
 print(matriz)
