66cb13d0d4ceaf386413c2633db01bb5180a8630
Juan Medina <91565820+jmedina28@users.noreply.github.com>
2022-09-23 14:47:27 +0200

    añado la funcion que rellena cada fila de la matriz con 3 elementos aleatorios

--- ejer1.py
+++ ejer1.py
@@ -4,6 +4,14 @@ matriz = []
 fila = 0
 dimensiones = int(input("Introduzca el número de filas que desea que tenga su matriz ix4: "))
 
-for i in range(dimensiones):
+
+def generador(dim):
+  
+  for i in range(dim):
     matriz.append([])
+    for j in range(3):
+      matriz[i].append(random.randint(0,10))
+   
+    
+generador(dimensiones)
 print(matriz)
