0822d3f3ea93ba23a4ccc6cf31dfca5a793b0961
Juan Medina <91565820+jmedina28@users.noreply.github.com>
2022-09-23 15:08:57 +0200

    añadido ejercicio 1 en lanzador

--- lanzador.py
+++ lanzador.py
@@ -1,3 +1,12 @@
-from ejer1 import *
-from ejer2 import *
-from ejer3 import *
+
+
+def lanzador():  
+    seleccion = int(input("Introduzca el número de ejercicio que desea ejecutar: "))
+    if seleccion == 1:
+        from ejer1 import generador1, elemento
+        matriz = []
+        fila = 0
+        dimensiones = int(input("Introduzca el número de filas que desea que tenga su matriz ix4: "))
+        generador1(dimensiones)
+        elemento(matriz)
+        print(matriz)
