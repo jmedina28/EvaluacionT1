3632fab4de7cb758dfac7c67dbdcd64806175fa5
Juan Medina <91565820+jmedina28@users.noreply.github.com>
2022-09-23 14:56:54 +0200

    añadida la segunda lista

--- ejer3.py
+++ ejer3.py
@@ -4,5 +4,8 @@ def generador(opcion):
   if opcion == 1:
     for i in range(11):
       lista.append(i)
-    print(lista)
-generador(1)
+  elif opcion  == 2:
+    for i in range(-10,1):
+      lista.append(i)
+  return(lista)
+print(generador(2))
