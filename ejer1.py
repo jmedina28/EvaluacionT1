807487f27fcc561db76cffa75bac3c3c98e87356
Juan Medina <91565820+jmedina28@users.noreply.github.com>
2022-09-23 14:45:02 +0200

    añado el bucle que genera el numero de filas

---
+++ ejer1.py
@@ -0,0 +1,9 @@
+import random
+
+matriz = []
+fila = 0
+dimensiones = int(input("Introduzca el número de filas que desea que tenga su matriz ix4: "))
+
+for i in range(dimensiones):
+    matriz.append([])
+print(matriz)
